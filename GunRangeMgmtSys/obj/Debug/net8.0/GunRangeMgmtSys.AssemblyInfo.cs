9c10c4d2c9f7eaaad3c776b8cacfd4ce96b794dd
JBitar <JBitar@anaheim.net>
2024-04-08 09:27:42 -0700

    Added styling to the search form on inventory page

--- GunRangeMgmtSys/obj/Debug/net8.0/GunRangeMgmtSys.AssemblyInfo.cs
+++ GunRangeMgmtSys/obj/Debug/net8.0/GunRangeMgmtSys.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GunRangeMgmtSys")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3202335e5d3158fcc0607ec53508b464ab4392e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4345ea7a72296b5f3685820abf5e126de93607f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("GunRangeMgmtSys")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GunRangeMgmtSys")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
