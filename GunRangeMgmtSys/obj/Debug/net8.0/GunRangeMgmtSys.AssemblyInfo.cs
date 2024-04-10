887c7928bb084a0f38be1f7d5833eeaca32ac5e2
JBitar <JBitar@anaheim.net>
2024-04-10 08:55:30 -0700

    Added red highlight to dates out of 90days of compliance from last date at gunrange

--- GunRangeMgmtSys/obj/Debug/net8.0/GunRangeMgmtSys.AssemblyInfo.cs
+++ GunRangeMgmtSys/obj/Debug/net8.0/GunRangeMgmtSys.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GunRangeMgmtSys")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f5cb7de195f4b4c6bca25fae0eebf39953dd163")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2f79f100cc1e7ab5e6d50566b797b8d286365d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GunRangeMgmtSys")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GunRangeMgmtSys")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
